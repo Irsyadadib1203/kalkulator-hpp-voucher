974c971626e810ba660e1e30987fe1f1ca193a46
irsyadadib1203 <irsyadadib1234@gmail.com>
2026-07-21 07:27:19 +0700

    push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,11 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
+        <!-- Username -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="username" :value="__('Username')" />
+                <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Password -->
@@ -38,12 +38,6 @@
     </x-primary-button>
 
     <div class="mt-4 flex flex-col items-center gap-2">
-        @if (Route::has('password.request'))
-            <a href="{{ route('password.request') }}"
-               class="text-sm text-gray-500 hover:text-indigo-600 transition">
-                Forgot your password?
-            </a>
-        @endif
 
         @if (Route::has('register'))
             <p class="text-sm text-gray-500">
